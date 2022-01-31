6b4dd7b6f55d05ee56c094d062c5652b35d19813
Chelsea <schniderchelsea025@gmail.com>
2022-01-31 17:00:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import Token from './artifacts/contracts/Token.sol/Token.json'
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Update with the contract address logged out to the CLI when it was deployed 
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const tokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
 function App() {
+
+  // store greeting in local state
+  const [greeting, setGreetingValue] = useState('')
+  const [userAccount, setUserAccount] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log({ provider })
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  }
+
+  // call the smart contract, send an update
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider })
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      setGreetingValue('')
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+  async function sendCoins() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transation = await contract.transfer(userAccount, amount);
+      await transation.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className='btn btn-danger w-50 mb-3' onClick={fetchGreeting}>Fetch Greeting</button>
+        <button className='btn btn-info w-50 mb-3' onClick={setGreeting}>Set Greeting</button>
+        <input className='text-center w-50 fs-6' onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+
+        <br />
+
+        <button className='btn btn-warning w-50 mb-3' onClick={getBalance}>Get Balance</button>
+        <button className='btn btn-success w-50 mb-3' onClick={sendCoins}>Send Coins</button>
+        <input className='text-center w-50 fs-6 mb-3' onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+        <input className='text-center w-50 fs-6' onChange={e => setAmount(e.target.value)} placeholder="Amount" />
       </header>
     </div>
   );
